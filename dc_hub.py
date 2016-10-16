4b79ec885bea4c32f9c1b297eaf55f7f20b41bfb
madan96 <rishabhmadan96@gmail.com>
2016-10-16 08:00:59 +0530

    made PEP8 compliance

--- dc_hub.py
+++ dc_hub.py
@@ -1,9 +1,11 @@
 from facepy import GraphAPI
 import re
 import os
-def get_hub_add() :
-	graph = GraphAPI(os.environ['FB_ACCESS_TOKEN'])
-	query = "hhfhkgp?fields=website"
-	hub_website = graph.get(query)['website']
-	x = re.search(r'\d+\.\d+\.\d+\.\d+',hub_website)
-	return x.group()
+
+
+def get_hub_add():
+    graph = GraphAPI(os.environ['FB_ACCESS_TOKEN'])
+    query = "hhfhkgp?fields=website"
+    hub_website = graph.get(query)['website']
+    x = re.search(r'\d+\.\d+\.\d+\.\d+', hub_website)
+    return x.group()
